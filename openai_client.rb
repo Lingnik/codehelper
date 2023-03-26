372cc76ffcb103ba88640970ccf87d10ac249481
Taylor Meek <Lingnik@users.noreply.github.com>
2023-03-25 21:39:56 -0700

    attempt 5: retry if code block backticks are unbalanced

--- openai_client.rb
+++ openai_client.rb
@@ -9,30 +9,44 @@ class OpenAIClient
     @model = model
   end
 
-  def generate_code(prompt, max_tokens = 150, n = 1, stop = nil, temperature = 0.5)
-    response = @client.chat(parameters: {
-      model: @model,
-      messages: [
-        {role: "system", content: "You are a helpful assistant that translates English descriptions into Ruby code."},
-        {role: "user", content: "#{prompt}"}
-      ],
-      max_tokens: max_tokens,
-      n: n,
-      stop: stop,
-      temperature: temperature
-    })
+  def generate_code(prompt, max_tokens = 150, n = 1, stop = nil, temperature = 0.5, max_attempts = 5)
+    attempt = 1
   
-    content = response["choices"][0]["message"]["content"]
-    code_blocks = content.scan(/```ruby(.*?)```/m).flatten
-    extracted_code = code_blocks.map(&:strip).join("\n\n")
+    while attempt <= max_attempts
+      response = @client.chat(parameters: {
+        model: @model,
+        messages: [
+          {role: "system", content: "You are a helpful assistant that translates English descriptions into Ruby code."},
+          {role: "user", content: "#{prompt}"}
+        ],
+        max_tokens: max_tokens,
+        n: n,
+        stop: stop,
+        temperature: temperature
+      })
   
-    if ENV['DEBUG'] == 'true'
-      puts "---- Full Response Message Content ----"
-      puts content
-      puts "---- Extracted Code Blocks ----"
-      puts extracted_code
+      content = response["choices"][0]["message"]["content"]
+      backticks_count = content.scan(/```/).count
+  
+      if backticks_count % 2 == 0
+        code_blocks = content.scan(/```ruby\n(.*?)\n```/m).flatten
+        extracted_code = code_blocks.map(&:strip).join("\n\n")
+  
+        if ENV['DEBUG'] == 'true'
+          puts "---- Full Response Message Content ----"
+          puts content
+          puts "---- Extracted Code Blocks ----"
+          puts extracted_code
+        end
+  
+        return extracted_code
+      else
+        attempt += 1
+        puts "Unbalanced triple backticks detected. Retrying (attempt #{attempt})..." if ENV['DEBUG'] == 'true'
+      end
     end
   
-    extracted_code
+    raise "Unable to generate code with balanced triple backticks after #{max_attempts} attempts."
   end
+
 end
