f41a91c5e2349f54e9f85c69a1be6410427c37bd
Taylor Meek <Lingnik@users.noreply.github.com>
2023-03-27 01:16:23 -0700

    rdocs for remaining clients

--- git_client.rb
+++ git_client.rb
@@ -1,7 +1,24 @@
 require 'fileutils'
 
+##
+# The GitClient class provides an interface for interacting with Git repositories.
+#
+# This class provides several methods for cloning repositories, committing changes, creating and pushing branches, 
+# and committing and pushing changes.
+#
+# Example usage:
+#     client = GitClient.new
+#     repo_path = client.clone_repository(username, repo_name)
+#     client.commit_and_push(repo_path, issue_number, commit_message_file_path)
 class GitClient
 
+  ##
+  # Clones a Git repository from GitHub to a local directory.
+  #
+  # @param [String] username The owner of the repository.
+  # @param [String] repo_name The name of the repository.
+  #
+  # @return [String] The path to the local repository.
   def clone_repository(username, repo_name)
     repo_url = "git@github.com:#{username}/#{repo_name}.git"
     local_path = "tmp/#{repo_name}"
@@ -13,6 +30,11 @@ class GitClient
     local_path
   end
   
+  ##
+  # Commits changes to a Git repository.
+  #
+  # @param [String] repo_path The path to the local repository.
+  # @param [String] commit_message_file_path The path to the file containing the commit message.
   def git_commit(repo_path, commit_message_file_path)
     Dir.chdir(repo_path) do
       `git add .`
@@ -22,18 +44,34 @@ class GitClient
     end
   end
 
+  ##
+  # Creates a new Git branch for a given issue number.
+  #
+  # @param [String] local_repo_path The path to the local repository.
+  # @param [String] issue_number The issue number.
   def git_create_branch(local_repo_path, issue_number)
     Dir.chdir(local_repo_path) do
       system("git checkout -b issue_#{issue_number}")
     end
   end
 
+  ##
+  # Pushes changes to a Git repository.
+  #
+  # @param [String] local_repo_path The path to the local repository.
+  # @param [String] issue_number The issue number.
   def git_push(local_repo_path, issue_number)
     Dir.chdir(local_repo_path) do
       system("git push origin issue_#{issue_number}")
     end
   end
 
+  ##
+  # Commits and pushes changes to a Git repository.
+  #
+  # @param [String] local_repo_path The path to the local repository.
+  # @param [String] issue_number The issue number.
+  # @param [String] commit_message_file_path The path to the file containing the commit message.
   def commit_and_push(local_repo_path, issue_number, commit_message_file_path)
     git_create_branch(issue_number)
     git_commit(local_repo_path, commit_message_file_path)
--- github_client.rb
+++ github_client.rb
@@ -1,19 +1,41 @@
 require 'octokit'
 require 'json'
 
+##
+# The GitHubClient class provides an interface for interacting with the GitHub API.
 class GitHubClient
+  ##
+  # Initializes a new instance of the GitHubClient class.
+  #
+  # @param [String] access_token The GitHub access token to use for authentication.
   def initialize(access_token)
     @client = Octokit::Client.new(access_token: access_token)
   end
 
+  ##
+  # Retrieves the authenticated user.
+  #
+  # @return [Sawyer::Resource] The authenticated user.
   def get_authenticated_user
     @client.user
   end
 
+  ##
+  # Creates a new repository.
+  #
+  # @param [String] repo_name The name of the repository to create.
+  # @param [String] your_username The username of the user creating the repository.
+  # @param [Boolean] private_repo A boolean value indicating whether the repository should be private or public.
+  # @return [Sawyer::Resource] The newly created repository.
   def create_repository(repo_name, your_username, private_repo = true)
     @client.create_repository(repo_name, owner: your_username, private: private_repo)
   end
 
+  ##
+  # Creates issues for a given repository.
+  #
+  # @param [String] repo_full_name The full name of the repository to create issues for.
+  # @param [Array<String>] issue_titles An array of issue titles to create.
   def create_issues(repo_full_name, issue_titles)
     existing_issues = fetch_existing_issue_titles(repo_full_name)
 
@@ -25,30 +47,77 @@ class GitHubClient
     end
   end
 
+  ##
+  # Updates an issue in a given repository.
+  #
+  # @param [String] repo_full_name The full name of the repository containing the issue.
+  # @param [Integer] issue_number The number of the issue to update.
+  # @param [String] title The new title for the issue.
+  # @param [String] description The new description for the issue.
   def update_issue(repo_full_name, issue_number, title, description)
     @client.update_issue(repo_full_name, issue_number, title: title, body: description)
   end
 
+  ##
+  # Retrieves an issue from a given repository.
+  #
+  # @param [String] owner The owner of the repository.
+  # @param [String] repo The name of the repository.
+  # @param [Integer] issue_number The number of the issue to retrieve.
+  # @return [Sawyer::Resource] The retrieved issue.
   def get_issue(owner, repo, issue_number)
     @client.issue("#{owner}/#{repo}", issue_number)
   end
 
+  ##
+  # Retrieves issues from a given repository.
+  #
+  # @param [String] repo_full_name The full name of the repository to retrieve issues from.
+  # @param [String] state The state of the issues to retrieve.
+  # @return [Array<Sawyer::Resource>] An array of issues.
   def get_issues(repo_full_name, state: 'open')
     @client.issues(repo_full_name, state: state)
   end
 
+  ##
+  # Retrieves approved issues from a given repository.
+  #
+  # @param [String] repo_full_name The full name of the repository to retrieve issues from.
+  # @param [String] tag The tag to filter the issues.
+  # @return [Array<Sawyer::Resource>] An array of issues.
   def get_approved_issues(repo_full_name, tag = 'approved-for-codehelper')
     @client.issues(repo_full_name, labels: tag)
   end
 
+  ##
+  # Deletes the specified branch from the repository.
+  #
+  # @param [String] repo_full_name The full name of the repository (e.g., "username/repo").
+  # @param [String] branch The name of the branch to delete.
   def delete_branch(repo_full_name, branch)
     @client.delete_branch(repo_full_name, branch)
   end
 
+  ##
+  # Creates a pull request on the specified repository.
+  #
+  # @param [String] repo_full_name The full name of the repository (e.g., "username/repo").
+  # @param [String] head_branch The name of the branch you want to merge into the base branch.
+  # @param [String] base_branch The name of the branch you want your changes pulled into.
+  # @param [String] title The title of the pull request.
+  # @return [Sawyer::Resource] The newly created pull request.
   def create_pull_request(repo_full_name, head_branch, base_branch, title)
     @client.create_pull_request(repo_full_name, base_branch, head_branch, title)
   end
 
+  ##
+  # Creates a pull request for the specified issue on the specified repository.
+  #
+  # @param [String] repo The name of the repository.
+  # @param [Integer] issue_number The issue number.
+  # @param [String] base The name of the branch you want your changes pulled into.
+  # @param [String] head The name of the branch you want to merge into the base branch.
+  # @param [Integer] retries The maximum number of retries if the request fails (default: 5).
   def create_pull_request_for_issue(repo, issue_number, base, head, retries = 5)
     begin
       pull_request = @client.create_pull_request_for_issue(repo, base, head, issue_number)
@@ -69,11 +138,22 @@ class GitHubClient
 
   private
 
+  ##
+  # Fetches the existing issue titles for a given repository.
+  #
+  # @param [String] repo_full_name The full name of the repository.
+  # @return [Array<String>] The existing issue titles.
   def fetch_existing_issue_titles(repo_full_name)
     existing_issues = @client.list_issues(repo_full_name, state: 'all')
     existing_issues.map(&:title)
   end
 
+  ##
+  # Creates an issue for a given repository with retry functionality in case of a rate limit error.
+  #
+  # @param [String] repo_full_name The full name of the repository.
+  # @param [String] title The title of the issue.
+  # @param [Integer] max_retries The maximum number of retries to attempt.
   def create_issue_with_retry(repo_full_name, title, max_retries = 5)
     retries = 0
     begin
